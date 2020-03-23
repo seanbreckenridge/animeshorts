25a93d55dba37fb223a562a521bcad37d3f842b8
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-23 02:27:43 -0700

    remove cookie constants

--- site/html_generators/constants.py
+++ site/html_generators/constants.py
@@ -2,7 +2,6 @@
 LIST_REC_LINK = "index_rec.html"
 LIST_DATE_LINK = "index_newest.html"
 PEOPLE_LINK = "people.html"
-COOKIES_LINK = "cookies.html"
 SOURCES = "list_sources.yaml"
 PEOPLE_SOURCES = "people_sources.yaml"
 LIST_CSS = "list.css"
@@ -15,7 +14,6 @@ FULL_NAME = "Anime Shorts"
 SHORT_NAME = "AnimeShorts"
 LIST_TAB = "List"
 PEOPLE_TAB = "People"
-COOKIES_TAB = "Cookies"
 
 
 class order:
