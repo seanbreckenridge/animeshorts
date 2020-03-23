adcce68961813332860b5c518715ad62a3bed0c4
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-23 02:21:22 -0700

    make sure generate works from any directory
    dont print debug messages for json cache

--- generate.sh
+++ generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd site/html_generators
+CUR_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+cd "$CUR_DIR/site/html_generators"
+
 python3 generate_list.py -d
 python3 generate_people_list.py
